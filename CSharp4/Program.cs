0dc2e09e53de1b29c453a74bd985e857647562da
alitalu7 <55026802+alitalu7@users.noreply.github.com>
2022-06-23 21:40:31 +0300

    Add project files.

---
+++ CSharp4/Program.cs
@@ -0,0 +1,14 @@
+﻿try
+{
+    Console.WriteLine("Bir sayı giriniz...");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Bir sayı giriniz...");
+    int b = Convert.ToInt32(Console.ReadLine());
+    int sonuc = a + b;
+    Console.WriteLine(sonuc);
+
+}
+catch
+{
+    Console.WriteLine("Bir hata oluştu...");
+}
